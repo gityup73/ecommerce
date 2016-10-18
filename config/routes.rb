52b0ab3f39020a23afa7ba66c05ef6d947e4800d
Jessica Swirat <soxfan1173@gmail.com>
2016-10-18 09:19:39 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'all' => 'storefront#all_items'
+
+  get 'categorical' =>'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
+  devise_for :users
+  resources :categories
+  resources :products
+
+  root 'storefront#all_items'
+
+  get 'categories' =>'categories#index'
+
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
